36547a08c426100afc9b8e644ba8c95ccf9d3bb2
Brad Cerasani <brad@blackpixel.com>
2020-03-22 10:47:06 -0500

    Add settings for breakpoints, fonts, colors, and transitions

---
+++ src/components/settings.js
@@ -0,0 +1,35 @@
+export const breakpoint = {
+  sm: '400px',
+  md: '768px',
+  lg: '1280px',
+  xl: '1440px',
+};
+
+export const font = {
+  family: {
+    serif: `Canela, Georgia, 'Times New Roman', Times, serif`,
+    sansSerif: `'Untitled Sans', 'Scto Grotesk A', sans-serif`,
+  },
+  size: {
+    body: '16px',
+  },
+};
+
+export const color = {
+  white: '#fafafa',
+  black: '#1a1a1a',
+  red: {
+    default: 'rgb(214, 68, 90)',
+    translucent: 'rgb(214, 68, 90, 0.5)',
+  },
+  cyan: {
+    default: 'rgb(117, 241, 257)',
+    translucent: 'rgb(117, 241, 257, 0.5)',
+  },
+};
+
+export const transition = {
+  fast: '150ms',
+  default: '250ms',
+  slow: '450ms',
+};

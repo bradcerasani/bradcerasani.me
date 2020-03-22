838c1aaa2929575a49cb05d16bfb0b44da37ba43
Brad Cerasani <brad@blackpixel.com>
2020-03-22 10:47:38 -0500

    Add basic responsive styles for Logo

--- src/components/atoms/logo.js
+++ src/components/atoms/logo.js
@@ -1,17 +1,21 @@
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 
+import { breakpoint } from '../settings';
+
 export const Logo = styled(Link)`
   font-family: Karbon, sans-serif;
-  font-size: 20px;
+  font-size: 16px;
   font-weight: 400;
-  letter-spacing: 2px;
+  letter-spacing: 0.15em;
   line-height: 1;
   margin: 0;
+  position: relative;
   text-decoration: none;
   text-transform: uppercase;
+  z-index: 10;
 
-  &:hover {
-    color: hsl(0, 0%, 36%, 1);
+  @media (min-width: ${breakpoint.sm}) {
+    font-size: 20px;
   }
 `;

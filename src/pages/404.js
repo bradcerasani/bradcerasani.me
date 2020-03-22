323945a9bed64fda9d6318d12665cd7a20b73e5a
Brad Cerasani <brad@blackpixel.com>
2020-03-22 10:46:13 -0500

    Update 404 to use Noise atom

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
+import { Noise } from '../components/atoms/noise';
 import Layout from '../components/layout';
 import Head from '../components/head';
 
@@ -13,92 +14,67 @@ function PageNotFound(props) {
       <Head title="404: Page not found">
         <style>
           {`
+            body {
+              background-color: #0000a1 !important;
+              color: #fafafa;
+              position: relative;
+              text-shadow: 0 0 0.5em #060b76;
+            }
 
-          @keyframes grain {
-            0%, 100% { transform:translate(0, 0) }
-            10% { transform:translate(-5%, -10%) }
-            20% { transform:translate(-15%, 5%) }
-            30% { transform:translate(7%, -25%) }
-            40% { transform:translate(-5%, 25%) }
-            50% { transform:translate(-15%, 10%) }
-            60% { transform:translate(15%, 0%) }
-            70% { transform:translate(0%, 15%) }
-            80% { transform:translate(3%, 35%) }
-            90% { transform:translate(-10%, 10%) }
-          }
+            body::after {
+              animation-timing-function: ease-in-out;
+              animation-delay: 800ms;
+              animation-direction: reverse;
+              animation-duration: 1s;
+              animation-fill-mode: forwards;
+              animation-name: fadeIn;
+              background-color: black;
+              background-image: url('/images/xp.gif');
+              background-position: center;
+              background-repeat: no-repeat;
+              background-size: contain;
+              content: '';
+              height: 100%;
+              left: 0;
+              pointer-events: none;
+              position: absolute;
+              top: 0;
+              width: 100%;
+              z-index: 10;
+            }
 
-          body {
-            background-color: #0000a1 !important;
-            color: #fafafa;
-            position: relative;
-            text-shadow: 0 0 0.5em #060b76;
-          }
+            body::before {
+              ${Noise}
+            }
 
-          body::after {
-            animation-timing-function: ease-in-out;
-            animation-delay: 800ms;
-            animation-direction: reverse;
-            animation-duration: 1s;
-            animation-fill-mode: forwards;
-            animation-name: fadeIn;
-            background-color: black;
-            background-image: url('/images/xp.gif');
-            background-position: center;
-            background-repeat: no-repeat;
-            background-size: contain;
-            content: "";
-            height: 100%;
-            left: 0;
-            pointer-events: none;
-            position: absolute;
-            top: 0;
-            width: 100%;
-            z-index: 10;
-          }
+            h2 {
+              font-family: 'Jetbrains Mono', monospace;
+              font-size: 56px;
+              font-weight: 400;
+              margin-top: 120px;
+              padding-top: calc(50vh - (316px + 32px + 120px));
+              text-transform: uppercase;
+              white-space: nowrap;
+            }
 
-          body::before {
-            animation: grain 8s steps(10) infinite;
-            background-image: url('/images/noise.png');
-            content: "";
-            display: block;
-            height: 300%;
-            left: -100%;
-            opacity: 0.6;
-            pointer-events: none;
-            position: fixed;
-            top: -100%;
-            width: 300%;
-            z-index: 5;
-          }
+            video {
+              display: block;
+              height: 100%;
+              left: 0;
+              margin-bottom: 0;
+              object-fit: cover;
+              opacity: 1;
+              position: absolute;
+              top: 0;
+              width: 100%;
+              z-index: -1;
+            }
 
-          h2 {
-            font-family: 'Jetbrains Mono', monospace;
-            font-size: 56px;
-            font-weight: 400;
-            margin-top: 120px;
-            padding-top: calc(50vh - (316px + 32px + 120px));
-            text-transform: uppercase;
-            white-space: nowrap;
-          }
-
-          video {
-            display: block;
-            height: 100%;
-            left: 0;
-            margin-bottom: 0;
-            object-fit: cover;
-            opacity: 1;
-            position: absolute;
-            top: 0;
-            width: 100%;
-            z-index: -1;
-          }
-
-          a,
-          a.active,
-          a:hover {
-            color: inherit !important;
-          }
+            a,
+            a.active,
+            a:hover {
+              color: inherit !important;
+            }
           `}
         </style>
       </Head>

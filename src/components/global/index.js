6afdca113fd83794b7f2eda87d1cd82b7614ce97
Brad Cerasani <brad@blackpixel.com>
2020-03-30 21:28:17 -0500

    Add noise to global body

--- src/components/global/index.js
+++ src/components/global/index.js
@@ -1,16 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
 import { modernNormalize } from './modern-normalize';
 
-import { font } from '../settings';
+import { font, breakpoint } from '../settings';
+import { Noise } from '../atoms/noise';
 
 export const GlobalStyle = createGlobalStyle`
   ${modernNormalize}
 
   html, body {
-    font-family: ${font.family.body};
+    font-family: ${font.family.sansSerif};
     font-size: ${font.size.body};
     height: 100%;
     scroll-behavior: smooth;
+
+    @media (min-width: ${breakpoint.md}) {
+      font-size: 20px;
+    }
   }
 
   body {
@@ -20,11 +25,26 @@ export const GlobalStyle = createGlobalStyle`
     transition-duration: 400ms;
     transition-property: background-color, color;
     transition-timing-function: ease-in-out;
+
+    &::after {
+      ${Noise}
+
+      opacity: 0.2;
+      z-index: 0;
+    }
+  }
+
+  h6 {
+    font-family: ${font.family.serif};
+    font-size: 1.15rem;
+    font-weight: 600;
+    letter-spacing: 0;
+    margin-bottom: 1rem;
+    margin-top: 0;
   }
 
   section {
     color: hsl(0, 0%, 20%);
-    font-size: 20px;
     line-height: 1.4;
     margin-bottom: 64px;
   }

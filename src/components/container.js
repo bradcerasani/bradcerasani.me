3ce8dc3534463a25bd77fc779683525d38b78ba4
Brad Cerasani <brad@blackpixel.com>
2020-03-30 21:28:02 -0500

    Tweak container max width

--- src/components/container.js
+++ src/components/container.js
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 const Container = styled.div`
   margin-left: auto;
   margin-right: auto;
-  max-width: 700px;
-  padding: 32px;
+  max-width: 720px;
 `;
 
 export default Container;

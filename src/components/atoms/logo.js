7a95593257cc60d7af22064cb851354ceb0ee393
Brad Cerasani <brad@blackpixel.com>
2020-03-30 21:27:18 -0500

    Use global sansSerif in "Logo"

--- src/components/atoms/logo.js
+++ src/components/atoms/logo.js
@@ -1,21 +1,10 @@
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 
-import { breakpoint } from '../settings';
-
 export const Logo = styled(Link)`
-  font-family: Karbon, sans-serif;
-  font-size: 16px;
-  font-weight: 400;
-  letter-spacing: 0.15em;
   line-height: 1;
   margin: 0;
   position: relative;
   text-decoration: none;
-  text-transform: uppercase;
   z-index: 10;
-
-  @media (min-width: ${breakpoint.sm}) {
-    font-size: 20px;
-  }
 `;

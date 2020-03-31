e1555e1ff1d00dce5179c4ac2fec5162e1dd401a
Brad Cerasani <brad@blackpixel.com>
2020-03-30 21:28:31 -0500

    Adjust spacing in header

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
 
+import { breakpoint } from '../settings';
+
 export const StyledHeader = styled.div`
   margin-bottom: 4rem;
-  margin-top: 2rem;
+  margin-top: 4rem;
+
+  @media (min-width: ${breakpoint.md}) {
+    margin-top: 5rem;
+  }
 `;
 
 export const HeroContainer = styled.div`

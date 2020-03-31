78b658e94b8a69c6296e3b91e52271167ab92902
Brad Cerasani <brad@blackpixel.com>
2020-03-30 21:43:49 -0500

    Update main navigation link images

--- src/components/header/links.js
+++ src/components/header/links.js
@@ -2,16 +2,16 @@ export const links = [
   {
     to: '/',
     label: 'Home',
-    imageSrc: '/images/nav/home.gif',
+    imageSrc: '/images/nav/home-2.gif',
   },
   {
     to: '/about/',
     label: 'About',
-    imageSrc: '/images/nav/about.jpg',
+    imageSrc: '/images/nav/about-80s.jpg',
   },
   {
     to: '/writing/',
     label: 'Writing',
-    imageSrc: '/images/nav/writing.gif',
+    imageSrc: '/images/nav/writing-2.gif',
   },
 ];

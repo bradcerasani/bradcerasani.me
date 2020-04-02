9fe38f8a5978479661dcf9489300424877dd3a77
Brad Cerasani <brad@blackpixel.com>
2020-04-01 20:00:57 -0500

    Edit max container width

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 720,
+              maxWidth: 730,
             },
           },
           `gatsby-remark-responsive-iframe`,

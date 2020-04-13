f5afb3577f35753d199788433ef5d1f3d57ef3bf
Brad Cerasani <brad@blackpixel.com>
2020-04-12 20:49:13 -0500

    Go to great lengths to prevent footnote backref emoji on iOS

--- src/components/_global/footnotes.js
+++ src/components/_global/footnotes.js
@@ -1,11 +1,15 @@
 import { css } from 'styled-components';
 
-import { color } from '../theme';
+import { breakpoint, color } from '../theme';
 
 const Footnotes = css`
   .footnotes {
     font-size: 0.8rem;
-    margin-bottom: 4rem;
+    margin-bottom: 3rem;
+
+    @media (min-width: ${breakpoint.md}) {
+      margin-bottom: 4rem;
+    }
   }
 
   .footnote-ref {
@@ -13,8 +17,13 @@ const Footnotes = css`
   }
 
   .footnotes hr {
-    margin-bottom: 4rem;
-    margin-top: 4rem;
+    margin-bottom: 3rem;
+    margin-top: 3rem;
+
+    @media (min-width: ${breakpoint.md}) {
+      margin-bottom: 4rem;
+      margin-top: 4rem;
+    }
   }
 
   .footnotes ol li {
@@ -28,13 +37,30 @@ const Footnotes = css`
     display: inline;
   }
 
-  .footnote-backref {
-    padding-left: 0.25rem;
-    text-decoration: none;
-    vertical-align: -1px;
+  .footnote-backref::before {
+    --aspectRatio: (4 / 5);
+    --height: 0.6rem;
+    background-image: url('data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMjAgMTYiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTUuMzcxIDE1LjM5di00LjE5NWg4LjgxM2MxLjUyMyAwIDIuODItLjUzNyAzLjg5LTEuNjExIDEuMDctMS4wNzQgMS42MDYtMi4zNyAxLjYwNi0zLjg4NSAwLTEuNTE1LS41MzUtMi44MS0xLjYwNi0zLjg4NS0xLjA3LTEuMDc0LTIuMzY3LTEuNjEtMy44OS0xLjYxaC0uMjgydjEuNzM0aC4yODJjMS4wNDYgMCAxLjkzNS4zNjcgMi42NjYgMS4xMDFhMy42MzQgMy42MzQgMCAwIDEgMS4wOTUgMi42NmMwIDEuMDQtLjM2NSAxLjkyNi0xLjA5NSAyLjY2LS43My43MzUtMS42MiAxLjEwMi0yLjY2NiAxLjEwMkg1LjM3VjUuMjY2TC4zMSAxMC4zMjhsNS4wNjIgNS4wNjN6IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48L3N2Zz4K');
+    background-repeat: no-repeat;
+    background-size: contain;
+    content: '';
+    display: inline-block;
+    height: var(--height);
+    margin-left: 0.25rem;
+    opacity: 0.5;
+    -webkit-tap-highlight-color: transparent;
+    transition-duration: 200ms;
+    transition-property: opacity;
+    transition-timing-function: ease-in-out;
+    visibility: visible;
+    width: calc(var(--height) / var(--aspectRatio));
+  }
 
-    &:hover {
-      color: ${color.black};
+  .footnote-backref {
+    visibility: hidden;
+
+    &:hover::before {
+      opacity: 1;
     }
   }
 `;

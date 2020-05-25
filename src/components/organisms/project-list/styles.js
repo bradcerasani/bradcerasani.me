182ad9ac0459e9337573ec819a0042d9c9f5c7ae
Brad Cerasani <brad@blackpixel.com>
2020-05-24 20:40:17 -0500

    Fix more lint errors

--- src/components/organisms/project-list/styles.js
+++ src/components/organisms/project-list/styles.js
@@ -1,24 +1,21 @@
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 
-import { breakpoint, color } from '../../theme';
-import { Intrinsic } from '../../atoms';
-
 export const StyledProjectList = styled.div``;
 
 export const StyledProjectItemImageWrapper = styled.div`
+  border: 6px double black;
+  margin-bottom: 1.5rem;
   margin-left: -10rem;
   margin-right: -10rem;
-  margin-bottom: 1.5rem;
-  border: 6px double black;
   padding: 1.5rem;
 `;
 
 export const StyledProjectItemDetailsWrapper = styled.div`
   h3 {
     font-size: 1.25rem;
-    text-decoration: none;
     position: relative;
+    text-decoration: none;
   }
 `;
 

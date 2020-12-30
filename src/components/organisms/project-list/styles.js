8ff7811fa1e672125edf61764188408b857cc48e
Brad Cerasani <brad@blackpixel.com>
2020-12-29 19:21:23 -0600

    Tweak ProjectList styles

--- src/components/organisms/project-list/styles.js
+++ src/components/organisms/project-list/styles.js
@@ -1,14 +1,29 @@
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 
+import { breakpoint, color, font, transition } from '../../theme';
+
 export const StyledProjectList = styled.div``;
 
 export const StyledProjectItemImageWrapper = styled.div`
   border: 6px double black;
   margin-bottom: 1.5rem;
-  margin-left: -10rem;
-  margin-right: -10rem;
-  padding: 1.5rem;
+  padding: 0.75rem;
+
+  figure {
+    margin: 0 !important;
+  }
+
+  @media (min-width: ${breakpoint.md}) {
+    margin-left: -4rem;
+    margin-right: -4rem;
+    padding: 1.5rem;
+  }
+
+  @media (min-width: ${breakpoint.lg}) {
+    margin-left: -10rem;
+    margin-right: -10rem;
+  }
 `;
 
 export const StyledProjectItemDetailsWrapper = styled.div`
@@ -21,7 +36,11 @@ export const StyledProjectItemDetailsWrapper = styled.div`
 
 export const StyledProjectItem = styled(Link)`
   display: block;
-  margin-bottom: 6rem;
+  margin-bottom: 3rem;
   position: relative;
   text-decoration: none;
+
+  @media (min-width: ${breakpoint.md}) {
+    margin-bottom: 6rem;
+  }
 `;

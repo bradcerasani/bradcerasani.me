2b9cda4d598bfa6539eb4dbbf9b0b5ad42d2037b
Brad Cerasani <brad@blackpixel.com>
2021-01-02 16:34:55 -0600

    Deprecate /writing and /projects paths

--- src/components/organisms/header/links.js
+++ src/components/organisms/header/links.js
@@ -1,17 +1,12 @@
 export const links = [
   {
-    to: '/about/',
-    label: 'About',
-    imageSrc: '/images/nav/about-80s.jpg',
-  },
-  {
-    to: '/writing/',
-    label: 'Writing',
+    to: '/',
+    label: 'Index',
     imageSrc: '/images/gifs/writing.gif',
   },
   {
-    to: '/',
-    label: 'Projects',
-    imageSrc: '/images/gifs/home.gif',
+    to: '/about/',
+    label: 'About',
+    imageSrc: '/images/nav/about-80s.jpg',
   },
 ];

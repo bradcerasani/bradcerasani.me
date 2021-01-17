48875a014d31454e5992e329e19b6206de630a4d
Brad Cerasani <brad@blackpixel.com>
2021-01-17 10:39:01 -0600

    Wrap lazysizes import in client conditional

--- src/components/templates/layout.js
+++ src/components/templates/layout.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
-import 'lazysizes';
-import 'lazysizes/plugins/unveilhooks/ls.unveilhooks';
+
+if (typeof window === 'object') {
+  import 'lazysizes';
+  import 'lazysizes/plugins/unveilhooks/ls.unveilhooks';
+}
 
 import { Container, Note } from 'src/components/atoms';
 import { Footer, Header } from 'src/components/organisms';

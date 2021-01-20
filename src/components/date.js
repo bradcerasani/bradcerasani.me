018f6c98aa30bdb6cc4fc59c96582a506245ef66
Brad Cerasani <brad@blackpixel.com>
2021-01-19 21:04:17 -0600

    Make optional vertical Date variant default

--- src/components/date.js
+++ src/components/date.js
@@ -2,30 +2,21 @@ import styled, { css } from 'styled-components';
 
 import { breakpoint } from 'src/settings';
 
-const verticalOrientation = css`
-  margin-top: -1.75rem;
-  position: absolute;
-
-  @media (min-width: ${breakpoint.md}) {
-    margin-left: -2rem;
-    margin-top: 0;
-    transform: rotate(-90deg) translateY(-8ch);
-    transform-origin: 100%;
-  }
-`;
-
 export const Date = styled.div`
   color: inherit;
   font-family: var(--fontFamilySansSerif);
   font-size: 0.75rem;
   font-weight: 500;
   letter-spacing: 0.05em;
+  margin-top: -1.75rem;
+  position: absolute;
   text-transform: uppercase;
 
-  /* stylelint-disable-next-line */
-  ${(props) => props.orientation === 'vertical' && verticalOrientation}
-
-  @media(min-width: ${breakpoint.md}) {
+  @media (min-width: ${breakpoint.md}) {
     font-size: 0.6rem;
+    margin-left: calc(-1 * var(--spaceDefault));
+    margin-top: 0;
+    transform-origin: 100%;
+    transform: rotate(-90deg) translateY(-8ch);
   }
 `;

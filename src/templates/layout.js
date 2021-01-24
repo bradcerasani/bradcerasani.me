08934caa3b522e9f7737c7017b81ee470e5a5355
Brad Cerasani <brad@blackpixel.com>
2021-01-24 10:03:18 -0600

    Fix lazySizes import

--- src/templates/layout.js
+++ src/templates/layout.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-import 'lazysizes';
+import lazySizes from 'lazysizes';
 import 'lazysizes/plugins/unveilhooks/ls.unveilhooks';
 
 lazySizes.cfg.preloadAfterLoad = true;

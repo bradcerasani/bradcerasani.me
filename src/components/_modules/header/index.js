8423599312828df716e8dbd7c23a2f7b9159685b
Brad Cerasani <bradcerasani@gmail.com>
2022-03-02 20:00:08 -0600

    Reduce layout shift on initial load

--- src/components/_modules/header/index.js
+++ src/components/_modules/header/index.js
@@ -21,6 +21,7 @@ export const Header = (props) => {
   const allMdx = null;
 
   const { date, headline = 'Design & Engineering' } = props;
+  const [mounted, setMounted] = useState(false);
   const [isVisible, setVisibility] = useState(false);
   const [overlayTransitioned, setOverlayTransitioned] = useState(false);
   const [showSocial, setShowSocial] = useState(false);
@@ -37,7 +38,7 @@ export const Header = (props) => {
     : null;
 
   useEffect(() => {
-    console.log('mounted');
+    setMounted(true);
 
     // Lock scroll when Nav overlay is visible
     document.body.style.overflow = isVisible ? 'hidden' : 'scroll';
@@ -56,82 +57,86 @@ export const Header = (props) => {
 
   return (
     <>
-      <MobileNavOverlay
-        $isVisible={isVisible}
-        onTransitionEnd={() => {
-          setOverlayTransitioned(isVisible ? true : false);
-          setShowSocial(isVisible);
-        }}
-      >
-        <MobileNavWrapper>
-          {links.map(({ to, label }, index) => (
-            <Link href={to} key={to} passHref>
-              <MobileNavItem
-                $isActive={router.pathname === to ? 'is-active' : ''}
-                $isVisible={overlayTransitioned}
-                onAnimationStart={() =>
-                  setShowSocial(index === links.length - 1)
-                }
-                style={{ animationDelay: `calc(${100 * index}ms)` }}
-              >
-                {label}
-              </MobileNavItem>
-            </Link>
-          ))}
+      {mounted && (
+        <MobileNavOverlay
+          $isVisible={isVisible}
+          onTransitionEnd={() => {
+            setOverlayTransitioned(isVisible ? true : false);
+            setShowSocial(isVisible);
+          }}
+        >
+          <MobileNavWrapper>
+            {links.map(({ to, label }, index) => (
+              <Link href={to} key={to} passHref>
+                <MobileNavItem
+                  $isActive={router.pathname === to ? 'is-active' : ''}
+                  $isVisible={overlayTransitioned}
+                  onAnimationStart={() =>
+                    setShowSocial(index === links.length - 1)
+                  }
+                  style={{ animationDelay: `calc(${100 * index}ms)` }}
+                >
+                  {label}
+                </MobileNavItem>
+              </Link>
+            ))}
 
-          <ul>
-            {allMdx &&
-              allMdx.edges.map(({ node }, index) => (
+            <ul>
+              {allMdx &&
+                allMdx.edges.map(({ node }, index) => (
+                  <MobileNavListItem
+                    key={node.fields.slug}
+                    $isVisible={showSocial}
+                    style={{
+                      animationDelay: `calc(${100 * (index + 1)}ms)`,
+                      textDecoration: 'underline',
+                    }}
+                  >
+                    <Link key={node.frontmatter.title} href={node.fields.slug}>
+                      {stripTags(node.frontmatter.title)}
+                    </Link>
+                  </MobileNavListItem>
+                ))}
+            </ul>
+
+            <ul>
+              {['Twitter', 'Instagram', 'GitHub'].map((link, index) => (
                 <MobileNavListItem
-                  key={node.fields.slug}
                   $isVisible={showSocial}
+                  key={link}
                   style={{
                     animationDelay: `calc(${100 * (index + 1)}ms)`,
-                    textDecoration: 'underline',
                   }}
                 >
-                  <Link key={node.frontmatter.title} href={node.fields.slug}>
-                    {stripTags(node.frontmatter.title)}
-                  </Link>
+                  <a
+                    href={`https://${link.toLowerCase()}.com/bradcerasani`}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    {link}
+                  </a>
                 </MobileNavListItem>
               ))}
-          </ul>
-
-          <ul>
-            {['Twitter', 'Instagram', 'GitHub'].map((link, index) => (
-              <MobileNavListItem
-                $isVisible={showSocial}
-                key={link}
-                style={{
-                  animationDelay: `calc(${100 * (index + 1)}ms)`,
-                }}
-              >
-                <a
-                  href={`https://${link.toLowerCase()}.com/bradcerasani`}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  {link}
-                </a>
-              </MobileNavListItem>
-            ))}
-          </ul>
-        </MobileNavWrapper>
-      </MobileNavOverlay>
+            </ul>
+          </MobileNavWrapper>
+        </MobileNavOverlay>
+      )}
 
       <StyledHeader
         itemscope="itemscope"
         itemtype="https://schema.org/SiteNavigationElement"
       >
-        <MobileNavMenuWrapper>
-          <Link href="/" passHref>
-            <Logo>Brad Cerasani</Logo>
-          </Link>
-          <MobileNavMenu
-            onClick={() => setVisibility(!isVisible)}
-            $isActive={isVisible}
-          />
-        </MobileNavMenuWrapper>
+        {mounted && (
+          <MobileNavMenuWrapper>
+            <Link href="/" passHref>
+              <Logo>Brad Cerasani</Logo>
+            </Link>
+            <MobileNavMenu
+              onClick={() => setVisibility(!isVisible)}
+              $isActive={isVisible}
+            />
+          </MobileNavMenuWrapper>
+        )}
 
         <HeroContainer>
           <h1>
@@ -148,7 +153,7 @@ export const Header = (props) => {
                     $showReturn={index === 0 && props.showReturn}
                   >
                     {label}
-                    <NavImage src={src} alt={alt} />
+                    {mounted && <NavImage src={src} alt={alt} />}
                   </NavItem>
                 </Link>
               );

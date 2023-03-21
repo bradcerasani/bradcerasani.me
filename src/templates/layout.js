c3891db059831fc77a3abf0a2f2b044cc5f9f093
Brad Cerasani <bradcerasani@gmail.com>
2023-03-21 18:02:40 -0500

    Enable lazySizes preloadAfterLoad

--- src/templates/layout.js
+++ src/templates/layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import lazySizes from 'lazysizes';
 
 import { Container, Footer, Header } from 'src/components';
 
-if (process.browser) {
+if (typeof window !== 'undefined') {
   require('lazysizes/plugins/unveilhooks/ls.unveilhooks');
+  lazySizes.cfg.preloadAfterLoad = true;
 }
 
 function Layout({ children, ...props }) {

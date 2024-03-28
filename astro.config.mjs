8c5489b60806fb7223de55d5a821a765c61d7526
Brad Cerasani <bradcerasani@gmail.com>
2024-03-28 15:22:17 -0500

    Kill Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import react from '@astrojs/react';
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), react()],
   redirects: {
@@ -10,9 +11,15 @@ export default defineConfig({
     '/writing': '/',
     '/projects': '/',
     '/tesla': '/writing/tesla-software-problems-in-winter',
-    '/projects/home-renovation': '/projects/designing-a-home',
+    '/projects/home-renovation': '/projects/designing-a-home'
   },
   markdown: {
-    syntaxHighlight: 'prism',
+    syntaxHighlight: 'prism'
   },
+  output: "server",
+  adapter: vercel(
+    {webAnalytics:{
+      enabled:true,
+    }}
+  )
 });
